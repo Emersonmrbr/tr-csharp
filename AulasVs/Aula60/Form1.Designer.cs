8bc7d0e4bc8515f661b71a9207f044b5086e20a6
Emersonmrbr <emersonmrbr@gmail.com>
2023-12-20 19:15:44 -0300

    Aula 60

--- AulasVs/Aula60/Form1.Designer.cs
+++ AulasVs/Aula60/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Aula60
 {
-    partial class Form1
+    partial class Janela1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,35 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_Send = new System.Windows.Forms.Button();
+            this.bt_Send = new System.Windows.Forms.Button();
+            this.lb_NomeCurso = new System.Windows.Forms.Label();
+            this.tb_Nome = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // btn_Send
+            // bt_Send
             // 
-            this.btn_Send.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btn_Send.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Send.Location = new System.Drawing.Point(62, 88);
-            this.btn_Send.Name = "btn_Send";
-            this.btn_Send.Size = new System.Drawing.Size(143, 56);
-            this.btn_Send.TabIndex = 0;
-            this.btn_Send.Text = "Send";
-            this.btn_Send.UseVisualStyleBackColor = false;
+            this.bt_Send.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt_Send.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Send.Location = new System.Drawing.Point(62, 56);
+            this.bt_Send.Name = "bt_Send";
+            this.bt_Send.Size = new System.Drawing.Size(143, 56);
+            this.bt_Send.TabIndex = 0;
+            this.bt_Send.Text = "Send";
+            this.bt_Send.UseVisualStyleBackColor = false;
             // 
-            // Form1
+            // lb_NomeCurso
+            // 
+            this.lb_NomeCurso.AutoSize = true;
+            this.lb_NomeCurso.Location = new System.Drawing.Point(62, 31);
+            this.lb_NomeCurso.Name = "lb_NomeCurso";
+            this.lb_NomeCurso.Size = new System.Drawing.Size(51, 13);
+            this.lb_NomeCurso.TabIndex = 1;
+            this.lb_NomeCurso.Text = "Curso C#";
+            // 
+            // tb_Nome
+            // 
+            this.tb_Nome.Location = new System.Drawing.Point(65, 129);
+            this.tb_Nome.Name = "tb_Nome";
+            this.tb_Nome.Size = new System.Drawing.Size(143, 20);
+            this.tb_Nome.TabIndex = 2;
+            // 
+            // Janela1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn_Send);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.tb_Nome);
+            this.Controls.Add(this.lb_NomeCurso);
+            this.Controls.Add(this.bt_Send);
+            this.Name = "Janela1";
+            this.Text = "Curso de C#";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.Button bt_Send;
+        private System.Windows.Forms.Label lb_NomeCurso;
+        private System.Windows.Forms.TextBox tb_Nome;
     }
 }
 
--- AulasVs/Aula60/Form1.cs
+++ AulasVs/Aula60/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Aula60
 {
-    public partial class Form1 : Form
+    public partial class Janela1 : Form
     {
-        public Form1()
+        public Janela1()
         {
             InitializeComponent();
         }
