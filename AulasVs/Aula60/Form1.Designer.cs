bab8450f8ee293a92fcbb62e77825822fb969404
Emersonmrbr <emersonmrbr@gmail.com>
2023-12-20 15:09:13 -0300

    Aula 60

--- AulasVs/Aula60/Form1.Designer.cs
+++ AulasVs/Aula60/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Send.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Send.Location = new System.Drawing.Point(62, 88);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(143, 56);
+            this.btn_Send.TabIndex = 0;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Send);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Send;
     }
 }
 
