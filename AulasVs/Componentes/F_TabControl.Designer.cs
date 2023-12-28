089c41d38f1e28eed7f087528e9bfa2c6db3c9f8
Emersonmrbr <54491594+Emersonmrbr@users.noreply.github.com>
2023-12-28 16:36:05 -0300

    Aula 94 tabcontrol e aula 95 table layout panel

--- AulasVs/Componentes/F_TabControl.Designer.cs
+++ AulasVs/Componentes/F_TabControl.Designer.cs
@@ -29,58 +29,159 @@
     private void InitializeComponent()
     {
       this.tabControl1 = new System.Windows.Forms.TabControl();
-      this.tabPage1 = new System.Windows.Forms.TabPage();
+      this.tab_Componentes = new System.Windows.Forms.TabPage();
       this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.Ttb_novaGuia = new System.Windows.Forms.TextBox();
+      this.Btn_novaGuia = new System.Windows.Forms.Button();
+      this.Btn_RemoverGuiaAtual = new System.Windows.Forms.Button();
+      this.Nud_Guia = new System.Windows.Forms.NumericUpDown();
+      this.Btn_PosicionarGuia = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
       this.tabControl1.SuspendLayout();
+      this.tab_Componentes.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.Nud_Guia)).BeginInit();
       this.SuspendLayout();
       // 
       // tabControl1
       // 
-      this.tabControl1.Controls.Add(this.tabPage1);
+      this.tabControl1.Controls.Add(this.tab_Componentes);
       this.tabControl1.Controls.Add(this.tabPage2);
-      this.tabControl1.Location = new System.Drawing.Point(12, 117);
+      this.tabControl1.Location = new System.Drawing.Point(12, 69);
       this.tabControl1.Name = "tabControl1";
       this.tabControl1.SelectedIndex = 0;
-      this.tabControl1.Size = new System.Drawing.Size(776, 321);
+      this.tabControl1.Size = new System.Drawing.Size(776, 369);
       this.tabControl1.TabIndex = 0;
       // 
-      // tabPage1
+      // tab_Componentes
       // 
-      this.tabPage1.Location = new System.Drawing.Point(4, 22);
-      this.tabPage1.Name = "tabPage1";
-      this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage1.Size = new System.Drawing.Size(768, 295);
-      this.tabPage1.TabIndex = 0;
-      this.tabPage1.Text = "tabPage1";
-      this.tabPage1.UseVisualStyleBackColor = true;
+      this.tab_Componentes.Controls.Add(this.button1);
+      this.tab_Componentes.Controls.Add(this.textBox1);
+      this.tab_Componentes.Controls.Add(this.label1);
+      this.tab_Componentes.Location = new System.Drawing.Point(4, 22);
+      this.tab_Componentes.Name = "tab_Componentes";
+      this.tab_Componentes.Padding = new System.Windows.Forms.Padding(3);
+      this.tab_Componentes.Size = new System.Drawing.Size(768, 343);
+      this.tab_Componentes.TabIndex = 0;
+      this.tab_Componentes.Text = "Componentes";
+      this.tab_Componentes.UseVisualStyleBackColor = true;
       // 
       // tabPage2
       // 
       this.tabPage2.Location = new System.Drawing.Point(4, 22);
       this.tabPage2.Name = "tabPage2";
       this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage2.Size = new System.Drawing.Size(792, 424);
+      this.tabPage2.Size = new System.Drawing.Size(768, 343);
       this.tabPage2.TabIndex = 1;
       this.tabPage2.Text = "tabPage2";
       this.tabPage2.UseVisualStyleBackColor = true;
+      // 
+      // Ttb_novaGuia
+      // 
+      this.Ttb_novaGuia.Location = new System.Drawing.Point(16, 13);
+      this.Ttb_novaGuia.Name = "Ttb_novaGuia";
+      this.Ttb_novaGuia.Size = new System.Drawing.Size(120, 20);
+      this.Ttb_novaGuia.TabIndex = 1;
+      // 
+      // Btn_novaGuia
+      // 
+      this.Btn_novaGuia.Location = new System.Drawing.Point(143, 11);
+      this.Btn_novaGuia.Name = "Btn_novaGuia";
+      this.Btn_novaGuia.Size = new System.Drawing.Size(75, 23);
+      this.Btn_novaGuia.TabIndex = 2;
+      this.Btn_novaGuia.Text = "Nova guia";
+      this.Btn_novaGuia.UseVisualStyleBackColor = true;
+      this.Btn_novaGuia.Click += new System.EventHandler(this.Btn_novaGuia_Click);
+      // 
+      // Btn_RemoverGuiaAtual
+      // 
+      this.Btn_RemoverGuiaAtual.Location = new System.Drawing.Point(224, 11);
+      this.Btn_RemoverGuiaAtual.Name = "Btn_RemoverGuiaAtual";
+      this.Btn_RemoverGuiaAtual.Size = new System.Drawing.Size(75, 23);
+      this.Btn_RemoverGuiaAtual.TabIndex = 3;
+      this.Btn_RemoverGuiaAtual.Text = "Remover guia atual";
+      this.Btn_RemoverGuiaAtual.UseVisualStyleBackColor = true;
+      this.Btn_RemoverGuiaAtual.Click += new System.EventHandler(this.Btn_RemoverGuiaAtual_Click);
+      // 
+      // Nud_Guia
+      // 
+      this.Nud_Guia.Location = new System.Drawing.Point(16, 40);
+      this.Nud_Guia.Name = "Nud_Guia";
+      this.Nud_Guia.Size = new System.Drawing.Size(120, 20);
+      this.Nud_Guia.TabIndex = 4;
+      // 
+      // Btn_PosicionarGuia
+      // 
+      this.Btn_PosicionarGuia.Location = new System.Drawing.Point(143, 40);
+      this.Btn_PosicionarGuia.Name = "Btn_PosicionarGuia";
+      this.Btn_PosicionarGuia.Size = new System.Drawing.Size(75, 23);
+      this.Btn_PosicionarGuia.TabIndex = 5;
+      this.Btn_PosicionarGuia.Text = "Posicionar na guia";
+      this.Btn_PosicionarGuia.UseVisualStyleBackColor = true;
+      this.Btn_PosicionarGuia.Click += new System.EventHandler(this.Btn_PosicionarGuia_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(34, 124);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(35, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "label1";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(37, 141);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 1;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(37, 168);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
       // 
       // F_TabControl
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.Btn_PosicionarGuia);
+      this.Controls.Add(this.Nud_Guia);
+      this.Controls.Add(this.Btn_RemoverGuiaAtual);
+      this.Controls.Add(this.Btn_novaGuia);
+      this.Controls.Add(this.Ttb_novaGuia);
       this.Controls.Add(this.tabControl1);
       this.Name = "F_TabControl";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "F_TabControl";
+      this.Load += new System.EventHandler(this.F_TabControl_Load);
       this.tabControl1.ResumeLayout(false);
+      this.tab_Componentes.ResumeLayout(false);
+      this.tab_Componentes.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.Nud_Guia)).EndInit();
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
 
     private System.Windows.Forms.TabControl tabControl1;
-    private System.Windows.Forms.TabPage tabPage1;
+    private System.Windows.Forms.TabPage tab_Componentes;
     private System.Windows.Forms.TabPage tabPage2;
+    private System.Windows.Forms.TextBox Ttb_novaGuia;
+    private System.Windows.Forms.Button Btn_novaGuia;
+    private System.Windows.Forms.Button Btn_RemoverGuiaAtual;
+    private System.Windows.Forms.NumericUpDown Nud_Guia;
+    private System.Windows.Forms.Button Btn_PosicionarGuia;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox textBox1;
   }
 }
--- AulasVs/Componentes/F_TabControl.cs
+++ AulasVs/Componentes/F_TabControl.cs
@@ -16,5 +16,44 @@ namespace Componentes
     {
       InitializeComponent();
     }
+
+    private void Btn_novaGuia_Click(object sender, EventArgs e)
+    {
+      if (Ttb_novaGuia.Text != string.Empty)
+      {
+        TabPage pagina = new TabPage();
+        pagina.Text = Ttb_novaGuia.Text;
+        pagina.Name = Ttb_novaGuia.Text;
+        pagina.TabIndex = tabControl1.TabPages.Count;
+        tabControl1.TabPages.Add(pagina);
+        Ttb_novaGuia.Clear();
+        DefinirMaximo();
+      }
+      else
+      {
+        MessageBox.Show("Digite o nome da guia");
+        Ttb_novaGuia.Focus();
+      }
+    }
+
+    private void Btn_RemoverGuiaAtual_Click(object sender, EventArgs e)
+    {
+      tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+      DefinirMaximo();
+    }
+
+    private void Btn_PosicionarGuia_Click(object sender, EventArgs e)
+    {
+      tabControl1.SelectedIndex = Int32.Parse(Nud_Guia.Value.ToString());
+    }
+    private void DefinirMaximo()
+    {
+      Nud_Guia.Maximum = tabControl1.TabPages.Count - 1;
+    }
+
+    private void F_TabControl_Load(object sender, EventArgs e)
+    {
+      DefinirMaximo();
+    }
   }
 }
